ab416b5e3fc3da4570113601503b491c54559995
Jayesh_Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-02-04 23:05:49 +0530

    useRef

--- react-part3/src/main.jsx
+++ react-part3/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Chat from './Chat.jsx'
+import FocusInput from './FocusInput.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App />
+    <Chat /> */}
+    <FocusInput/>
   </StrictMode>,
 )
