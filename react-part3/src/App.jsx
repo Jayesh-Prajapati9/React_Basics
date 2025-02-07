1a1790838e4b8d45a8fa2608763ce3c6e3ef0520
Jayesh_Prajapati9 <jayeshprajapati.0904@gmail.com>
2025-02-07 14:05:40 +0530

    useRef State

--- react-part3/src/App.jsx
+++ react-part3/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// useRef is a reference to a value , such that when you changed the value the component does not RE-Renders like it does in case of useState
 
+function App() {
+  const inputRef = useRef();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      useRef Example: 
+      <input ref={inputRef} type="text" />
+      <button onClick={getInput} type="submit"></button>
     </>
   )
+
+  function getInput() {
+    inputRef.current().focus();
+  }
 }
 
 export default App
